4cbe209dd6acae11fea658c2ae0c994d7ffeb4f1
yogibagus <58897819+yogibagus@users.noreply.github.com>
2021-08-31 00:41:58 +0700

    fix payment for delete last payment id when create new payment request

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -15,10 +15,8 @@ class Webhook extends CI_Controller
     public function callback()
     {
         $data = file_get_contents('php://input');
-        // $action = json_decode($data, true);
+        $action = json_decode($data, false);
         $data_log['CONTENT_LOG'] = $data;
         $this->M_payment->insert_log_webhook($data_log);
-
-        echo $data;
     }
 }
--- application/modules/payment/views/checkout_page.php
+++ application/modules/payment/views/checkout_page.php
@@ -274,7 +274,7 @@
                         $('#pay-button').prop("disabled", true);
                         // add spinner to button
                         $('#pay-button').html(
-                            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+                            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading, please wait...`
                         );
                         return;
                     }
@@ -283,7 +283,7 @@
                     $('#pay-button').prop("disabled", true);
                     // add spinner to button
                     $('#pay-button').html(
-                        `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+                        `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading, please wait...`
                     );
                     return;
                 }
