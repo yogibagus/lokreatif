8bb1b1b055f35d16f13db22f6e2952c9051beb33
mahendradwipurwanto <mahendradwipurwanto@gmail.com>
2021-08-31 18:06:17 +0700

    Fix bug pay ammount and count team on participan

--- application/controllers/Email_template.php
+++ application/controllers/Email_template.php
@@ -1,13 +1,14 @@
 <?php
 
-class Cronjob extends CI_Controller{
+class Email_template extends CI_Controller{
     public function __construct(){
         parent::__construct();
     }
 
-    public function index(){
+    public function default_auth($message){
+    	$data['message']	= $message;
         $this->load->view('email/header');
-        $this->load->view('email/auth');
+        $this->load->view('email/auth', $data);
         $this->load->view('email/footer');
     }
 
--- application/views/email/footer.php
+++ application/views/email/footer.php
@@ -8,7 +8,7 @@
            <div style="margin: 32px 56px 0 56px">
                <div style="margin-top: 32px">
                    <img style="margin: auto;display: block;" src="https://i.ibb.co/XtvzJBX/icon-ts.png'" width="75px" height="auto" alt="DurianPay logo">
-                   <div style="text-align: center; font-size: 10px;">LO-KREATIF 2021</div>
+                   <div style="text-align: center; font-size: 10px; margin-top:10px">LO-KREATIF 2021</div>
                </div>
            </div>
        </div>
